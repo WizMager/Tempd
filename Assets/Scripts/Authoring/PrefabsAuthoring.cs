eee6f48bce23590fb17c001debd99426791b6c0e
WizMager <wizmager@gmail.com>
2026-07-11 21:38:41 +0300

    -cleanup packages, create few start things, create plan for lobby and network things

---
+++ Assets/Scripts/Authoring/PrefabsAuthoring.cs
@@ -0,0 +1,24 @@
+﻿using Components;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Authoring
+{
+    public class PrefabsAuthoring : MonoBehaviour
+    {
+        public GameObject playerPrefab;
+        
+        private class PrefabsAuthoringBaker : Baker<PrefabsAuthoring>
+        {
+            public override void Bake(PrefabsAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.None);
+                
+                AddComponent(entity, new PrefabsComponent
+                {
+                    Player = GetEntity(authoring.playerPrefab, TransformUsageFlags.Dynamic)
+                });
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Components/PrefabsComponent.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace Components
+{
+    public struct PrefabsComponent : IComponentData
+    {
+        public Entity Player;
+    }
+}
