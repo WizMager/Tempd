ff9a2146536e6920d9a7652ef4f98b20c1373908
WizMager <wizmager@gmail.com>
2026-07-12 20:06:41 +0300

    -add field for change nickname, start work on networking

---
+++ Assets/Scripts/Lobby/GameBootstrap.cs
@@ -0,0 +1,15 @@
+﻿using Unity.NetCode;
+
+namespace Lobby
+{
+    public class GameBootstrap : ClientServerBootstrap
+    {
+        public override bool Initialize(string defaultWorldName)
+        {
+            AutoConnectPort = 0;
+            CreateLocalWorld(defaultWorldName);
+            
+            return true;
+        }
+    }
+}
