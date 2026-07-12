821535392411a21ccb88193b141146ce5ded4648
WizMager <wizmager@gmail.com>
2026-07-12 20:33:30 +0300

    -complete(i hope) with ui for start game, add controller and fll with fields

---
+++ Assets/Scripts/Ui/StartGame/PlayerListElement.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ui.StartGame
+{
+    public class PlayerListElement : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _playerNameText;
+        [SerializeField] private Image _readyImage;
+
+        public void ChangePlayerName(string playerName)
+        {
+            _playerNameText.text = playerName;
+        }
+
+        public void ChangeReadyStatus(bool ready)
+        {
+            _readyImage.color = ready ? Color.green : Color.red;
+        }
+    }
+}
---
+++ Assets/Scripts/Ui/StartGame/StartGameController.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ui.StartGame
+{
+    public class StartGameController : MonoBehaviour
+    {
+        [SerializeField] private Button _hostButton;
+        [SerializeField] private Button _joinButton;
+        [SerializeField] private Button _enterIpButton;
+        [SerializeField] private Button _acceptNameButton;
+        [SerializeField] private Button _readyButton;
+        [SerializeField] private Button _startGameButton;
+
+        [SerializeField] private TMP_InputField _enterIpAddressInputField;
+        [SerializeField] private TMP_InputField _enterNameInputField;
+
+        [SerializeField] private GameObject _menuContainer;
+        [SerializeField] private GameObject _joinPopupContainer;
+        [SerializeField] private GameObject _lobbyContainer;
+        
+        [SerializeField] private Transform _playerListRoot;
+        [SerializeField] private PlayerListElement _playerListElementprefab;
+    }
+}
