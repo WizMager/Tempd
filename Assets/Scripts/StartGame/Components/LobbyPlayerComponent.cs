77c8c7d82c9dfb112d2740694cb37c608c2d3390
WizMager <wizmager@gmail.com>
2026-07-16 00:18:12 +0300

    -add component for sync name and isReady

---
+++ Assets/Scripts/StartGame/Components/LobbyPlayerComponent.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.NetCode;
+
+namespace StartGame.Components
+{
+    public struct LobbyPlayerComponent : IComponentData
+    {
+        [GhostField] public FixedString32Bytes Name;
+        [GhostField] public bool IsReady;
+    }
+}
